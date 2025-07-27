3862a1f3b29450c84709d3b4260173d50ff693c8
Abhishek <abhishek2061a@gmail.com>
2025-07-27 21:26:17 +0530

    Restructure the project from adding backend code

--- web/dev-dist/sw.js
+++ web/dev-dist/sw.js
@@ -82,7 +82,7 @@ define(['./workbox-54d0af47'], (function (workbox) { 'use strict';
     "revision": "3ca0b8505b4bec776b69afdba2768812"
   }, {
     "url": "index.html",
-    "revision": "0.gr006tmask8"
+    "revision": "0.oed4m5fbbng"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("index.html"), {
